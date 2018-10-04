025b1386764a506b67d00a5c6e79e7940290d79c
Marcin Karski <karski80@gmail.com>
2018-10-04 09:49:54 +0100

    Minor tweaks

--- NetworkLayer/Application/AppDelegate.swift
+++ NetworkLayer/Application/AppDelegate.swift
@@ -8,6 +8,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let viewController = ViewController()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+
         return true
     }
 
--- NetworkLayer/Networking/ApiResource.swift
+++ NetworkLayer/Networking/ApiResource.swift
@@ -2,17 +2,21 @@ import Foundation
 
 protocol ApiResource {
     associatedtype Model: Decodable
+    var methodPath: String { get }
     func makeModel(data: Data) -> Model
 }
 
 extension ApiResource {
-	var url: URL {
-		let base = "https://api.github.com/users/marcinkarski"
-		return URL(string: base)!
-	}
+    var url: URL {
+        let base = "https://api.github.com"
+        let user = "marcinkarski"
+        let url = base + methodPath + user
+        return URL(string: url)!
+    }
 }
 
 struct UserResource: ApiResource {
+    let methodPath = "/users/"
     func makeModel(data: Data) -> User? {
         let decoder = JSONDecoder()
         guard let user = try? decoder.decode(User.self, from: data) else {
